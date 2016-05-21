725f3d17ce23e7f521a71431393a7c75cf48d0dd
Mark-A-A <markawai01@gmail.com>
2016-05-21 18:45:50 -0400

    fixed controllers for search

--- public/ang-controllers/search.js
+++ public/ang-controllers/search.js
@@ -1,10 +1,15 @@
 //controller for searching
-var searchController = angular.module('searchController')
-searchController.controller('searchController', function($scope){
+
+
+angular.module('tollCalculator').controller('searchController', function($scope){
+  
   console.log($scope.origin)
   console.log($scope.destination)
+  
   var origin = $scope.origin;
+
   var destination = $scope.destination;
+
   $http.get('/search').then(function() {
     var directionsQuery = "https://maps.googleapis.com/maps/api/directions/json?origin="+ origin + "&destination="+ destination + "&key=" + process.env.tollKey;
   })
