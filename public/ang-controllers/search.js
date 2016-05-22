dae7bff94dabd7c3d36e2b835a1884f2e189a589
Jeffrey Shapiro <jeffreyhshapiro@gmail.com>
2016-05-21 20:42:34 -0400

    successful API call to google geocoding

--- public/ang-controllers/search.js
+++ public/ang-controllers/search.js
@@ -2,13 +2,14 @@
 
 angular.module('tollCalculator')
   .controller('searchController', function($scope, $http){
+
     $scope.searchQuerySubmit = function(){
       var origin = $scope.origin;
       var destination = $scope.destination;
       console.log(origin)
       console.log(destination)
-      $http.get('/search').then(function() {
-        var directionsQuery = "https://maps.googleapis.com/maps/api/directions/json?origin="+ origin + "&destination="+ destination + "&key=AIzaSyA-nftHnip6wJ1zX_SoBfmmhZmynfvk-u0";
+      $http.get('https://maps.googleapis.com/maps/api/geocode/json?address='  + origin + '&key=AIzaSyA-nftHnip6wJ1zX_SoBfmmhZmynfvk-u0').then(function(data) {
+        console.log(data)
       })
     }
 })
